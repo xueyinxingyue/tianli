bd87967e6df0202e4ffb5e50b380af44889c7437
master <425899311@qq.com>
2023-06-20 21:48:51 +0800

    添加接口

--- src/main/java/com/xueyin/tianli/controller/BookController.java
+++ src/main/java/com/xueyin/tianli/controller/BookController.java
@@ -6,11 +6,7 @@ import com.xueyin.tianli.common.Result;
 import com.xueyin.tianli.entity.Book;
 import com.xueyin.tianli.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -42,6 +38,27 @@ public class BookController {
     public Result bookInfo(@RequestParam Integer bookId) {
         Book book = bookService.getById(bookId);
         return Result.success("获取成功", book);
+    }
+
+    //图书添加
+    @PostMapping("insert")
+    public Result insert(@RequestBody Book book){
+        bookService.save(book);
+        return Result.success("成功添加");
+    }
+
+    //图书删除
+    @DeleteMapping("delete")
+    public Result delete(Integer id){
+        bookService.removeById(id);
+        return Result.success("成功删除");
+    }
+
+    //图书信息修改
+    @PutMapping("update")
+    public Result update(@RequestBody Book book){
+        bookService.updateById(book);
+        return Result.success("修改成功");
     }
 }
 
