86984730d99aba81bc7d080decadc07dd414706f
master <425899311@qq.com>
2023-06-25 22:43:37 +0800

    时区

--- src/main/java/com/xueyin/tianli/entity/Post.java
+++ src/main/java/com/xueyin/tianli/entity/Post.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -30,6 +31,7 @@ public class Post implements Serializable {
 
     private String content;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date time;
 
     private Integer authorId;
